ed7127a74bdc6fc4faaeba40a380d9f6129009e7
ishraqe <ishraqenonymous@gmail.com>
2017-10-20 20:44:34 +0600

    auth ready

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ var _ =require('lodash');
 
 var {mongoose} = require('./db/mongoose');
 var {User} = require('./model/User');
+var {authenticate} = require('./middleware/authenticate');
+
 
 var app = express();
 const port= 3000;
@@ -12,11 +14,6 @@ const port= 3000;
 app.use(bodyParser.json());
 
 app.post('/user/sign-up',(req, res)=>{
-    var email = req.email;
-});
-
-
-app.post('/user/login',(req, res)=> {
     var email = req.body.email;
     var first_name = req.body.first_name;
     var last_name = req.body.last_name;
@@ -28,12 +25,34 @@ app.post('/user/login',(req, res)=> {
     user.save().then(() => {
         return user.generateAuthToken();
     }).then((token) => {
-        res.header('x-auth', token).send(user);
+        var userInfo=_.pick(user,['email','username','first_name','last_name','tokens']);
+        res.header('x-auth', token).send(userInfo);
     }).catch((e) => {
         res.status(400).send(e);
     })
 });
 
+
+app.post('/users/login', (req, res) => {
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        // return user.generateAuthToken().then((token) => {
+        //     res.header('x-auth', token).send(user);
+        // });
+       return user.generateAuthToken().then((token)=>{
+            console.log(token);
+            res.header('x-auth',token).send(user);
+        });
+
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+});
 
 app.listen(port,()=>{
    console.log('started on this port 3000');
